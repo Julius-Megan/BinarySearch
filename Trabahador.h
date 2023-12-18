35fc4ba095ecd59b71f5842a7a5cbf155f0049c4
Julius Such <124233350+Julius-Megan@users.noreply.github.com>
2023-12-18 23:45:13 +0000

    Add Trabahador.h and .cpp files.

---
+++ Trabahador.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+using namespace std;
+class Trabahador
+{
+private:
+string name;
+int id;
+int age;
+string job;
+int year;
+public:
+Trabahador(string, int, int, string, int);
+void declareFound();
+int getID();
+};
